1dd9e81f888a520179f18d280f6e2c261c49fc03
chris48s <chris.shaw480@gmail.com>
2021-01-24 14:55:47 +0000

    try the release script

--- .github/actions/draft-release/entrypoint.sh
+++ .github/actions/draft-release/entrypoint.sh
@@ -2,4 +2,59 @@
 
 set -euxo pipefail
 
-echo "$REPO_NAME"
+# Last server-YYYY-MM-DD tag
+LASTTAG=$(git tag | grep server | tail -n 1)
+
+# Find the marker in CHANGELOG.md
+INSERTPOINT=$(grep -n "^\-\-\-$" CHANGELOG.md | cut -f1 -d:)
+INSERTPOINT=$((INSERTPOINT+1))
+
+# Generate a release name
+RELEASENAME="server-$(date --rfc-3339=date)"
+
+# Assemble changelog entry
+rm -f temp-changes.txt
+touch temp-changes.txt
+{
+    echo "## $RELEASENAME"
+    echo ""
+    git log "$LASTTAG"..HEAD --no-merges --oneline --pretty="format:- %s" --perl-regexp --author='^((?!dependabot).*)$'
+    echo $'\n'
+} >> temp-changes.txt
+
+# Write the changelog
+sed -i "${INSERTPOINT} r temp-changes.txt" CHANGELOG.md
+
+# cleanup
+rm temp-changes.txt
+
+# run prettier (to ensure the markdown file doesn't fail CI)
+npm ci
+npm run prettier
+
+# set up a git user
+git config user.name "release[bot]"
+git config user.email "actions@users.noreply.github.com"
+
+# commit + push changelog
+git checkout -b "$RELEASENAME"
+git add CHANGELOG.md
+git commit -m "Update Changelog"
+git push origin "$RELEASENAME"
+
+# submit a PR
+TITLE="Changelog for Release $RELEASENAME"
+PR_RESP=$(curl https://api.github.com/repos/"$REPO_NAME"/pulls \
+    -X POST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    --include \
+    --data '{"title": "'"$TITLE"'", "body": "'"$TITLE"'", "head": "'"$RELEASENAME"'", "base": "master"}')
+echo "$PR_RESP"
+
+# add the 'release' label to the PR
+PR_API_URL=$(echo "$PR_RESP" | jq ._links.issue.href)
+curl "$PR_API_URL" \
+    -X POST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    --include \
+    --data '{"labels":["release"]}'
